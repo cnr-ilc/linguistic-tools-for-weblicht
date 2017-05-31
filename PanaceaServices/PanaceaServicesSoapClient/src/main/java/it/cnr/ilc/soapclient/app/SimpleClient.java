8a8284fad0273cfe84c82ac8aac9c44921164b8c
riccardodg <riccardo.delgratta@ilc.cnr.it>
2017-05-31 16:22:31 +0200

    PanaceaServicesSoapClient init 31/05/2017

---
+++ PanaceaServices/PanaceaServicesSoapClient/src/main/java/it/cnr/ilc/soapclient/app/SimpleClient.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.cnr.ilc.soapclient.app;
+
+/**
+ *
+ * @author Riccardo Del Gratta &lt;riccardo.delgratta@ilc.cnr.it&gt;
+ */
+public class SimpleClient {
+    
+    public static void main(String[] args) {
+        
+        System.err.println("Hi");
+        
+    }
+    
+}
---
+++ PanaceaServices/PanaceaServicesSoapClient/src/main/java/it/cnr/ilc/soapclient/i/PanaceaService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.cnr.ilc.soapclient.i;
+
+import org.soaplab.clients.SoaplabBaseClient;
+
+/**
+ *
+ * @author Riccardo Del Gratta &lt;riccardo.delgratta@ilc.cnr.it&gt;
+ */
+public interface PanaceaService {
+    
+    public static final String __URL__="http://langtech3.ilc.cnr.it:8080/soaplab2-axis/services";
+    
+    /**
+     * run the specific service
+     */
+    public void runService();
+    
+    /**
+     * get the client from the soaplab endpoint
+     * @param endpoint 
+     * @return the soaplab client
+     */
+    public SoaplabBaseClient getClient(String endpoint);
+    
+}
