5b02ba08c487a44ef02472ec62a67ba44d462d76
riccardodg <riccardo.delgratta@ilc.cnr.it>
2017-06-22 12:06:31 +0200

    Ready for production

--- Tokenizer-Base-Service/src/main/java/it/cnr/ilc/tokenizer/app/TokenizerBaseServiceApp.java
+++ Tokenizer-Base-Service/src/main/java/it/cnr/ilc/tokenizer/app/TokenizerBaseServiceApp.java
@@ -12,23 +12,36 @@ import it.cnr.ilc.tokenizer.service.resources.ReadMeResource;
 import it.cnr.ilc.tokenizer.service.resources.TokenizerBaseResource;
 import it.cnr.ilc.tokenizer.service.resources.TokenizerKafResource;
 import it.cnr.ilc.tokenizer.service.resources.TokenizerTabResource;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * This class extends io.dropwizard.Application and uses TokenizerBaseServiceConf as main configuration factory
+ * This class extends io.dropwizard.Application and uses
+ * TokenizerBaseServiceConf as main configuration factory
+ *
  * @author Riccardo Del Gratta &lt;riccardo.delgratta@ilc.cnr.it&gt;
  */
 public class TokenizerBaseServiceApp extends Application<TokenizerBaseServiceConf> {
 
     /**
-     * You can execute as java -jar ABC.jar server [file].
-     * file specify the configuration file. If not provided the 8080 as port and the / as root context are used. 
-     * @param args the arguments provided. The first is server to start the server. 
-     * The second is optional and specifies the yaml configuration file. Usually this last one is under src/assembly/conf
+     * You can execute as java -jar ABC.jar server [file]. file specify the
+     * configuration file. If not provided the 8080 as port and the / as root
+     * context are used.
+     *
+     * @param args the arguments provided. The first is server to start the
+     * server. The second is optional and specifies the yaml configuration file.
+     * Usually this last one is under src/assembly/conf
      * @throws Exception The possible exception thrown during the process
      */
+    private static String CLASS_NAME = TokenizerBaseServiceApp.class
+            .getName();
+
     public static void main(String[] args) throws Exception {
-        
-        
+        String message;
+        message = String.format("Init App -%s-", "TokenizerBaseServiceApp");
+        Logger
+                .getLogger(CLASS_NAME).log(Level.INFO, message);
+
         new TokenizerBaseServiceApp().run(args);
     }
 
@@ -37,17 +50,24 @@ public class TokenizerBaseServiceApp extends Application<TokenizerBaseServiceCon
 
     }
 
-    
     /**
-     * You can use this method to register your resources. You can do it in two ways:
+     * You can use this method to register your resources. You can do it in two
+     * ways:
      * <ol>
      * <li> register a single instance when the server starts up.</li>
-     * <li> register a new instance each time the service is invoked (myclass.class)</li>
+     * <li> register a new instance each time the service is invoked
+     * (myclass.class)</li>
      * </ol>
-     * You should take care of thread safe[ln]ess in your code, essentially synchronizing the process method
+     * You should take care of thread safe[ln]ess in your code, essentially
+     * synchronizing the process method
      */
     @Override
     public void run(TokenizerBaseServiceConf t, Environment environment) throws Exception {
+        
+        String message;
+        message = String.format("Loading Resources -%s-", "");
+        Logger
+                .getLogger(CLASS_NAME).log(Level.INFO, message+"  "+environment.getApplicationContext().toString());
 
 //        NamedEntitiesResource namedEntitiesResource = new NamedEntitiesResource();
 //        ReferencesResource referencesResource = new ReferencesResource();
--- Tokenizer-Base-Service/src/main/java/it/cnr/ilc/tokenizer/service/resources/TokenizerKafResource.java
+++ Tokenizer-Base-Service/src/main/java/it/cnr/ilc/tokenizer/service/resources/TokenizerKafResource.java
@@ -7,9 +7,7 @@ package it.cnr.ilc.tokenizer.service.resources;
 
 import it.cnr.ilc.tokenizer.service.core.TokenizerKafCore;
 import it.cnr.ilc.tokenizer.utils.OutPutWriter;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -29,22 +27,26 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 
 /**
- * The base resource to register in the environment. Registering this resource, the service will be available at the path kaf/tokenizer
- * which is the root of the service(s).
+ * The base resource to register in the environment. Registering this resource,
+ * the service will be available at the path kaf/tokenizer which is the root of
+ * the service(s).
+ *
  * @author Riccardo Del Gratta &lt;riccardo.delgratta@ilc.cnr.it&gt;
  */
 @Path("kaf/tokenizer")
 public class TokenizerKafResource {
-    
+
     private static final String TEXT_XML = "text/xml";
     private static final String FALL_BACK_MESSAGE = "Data processing failed";
     private static final String TEMP_FILE_PREFIX = "tok-sent-output-temp";
     private static final String TEMP_FILE_SUFFIX = ".xml";
 
-    
-    
+    private String CLASS_NAME = TokenizerKafResource.class.getName();
+    private String context = "services/kaf";
+
     /**
      * This method tokenizes a plain text to produce a TAB document
+     *
      * @param lang the input language
      * @param input the input stream
      * @return the output file
@@ -54,22 +56,34 @@ public class TokenizerKafResource {
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(TEXT_XML)
     public StreamingOutput tokenizeTextFromPlain(@QueryParam("lang") String lang, final InputStream input) {
+        String message;
+        String routine = "tokenizeTextFromPlain";
+        message = String.format("Executing  -%s- in context -%s-", routine, context);
+        Logger
+                .getLogger(CLASS_NAME).log(Level.INFO, message);
         OutputStream tempOutputData = null;
         File tempOutputFile = null;
         try {
             tempOutputFile = File.createTempFile(TEMP_FILE_PREFIX, TEMP_FILE_SUFFIX);
-            
+
         } catch (IOException ex) {
             if (tempOutputData != null) {
                 try {
                     tempOutputData.close();
                 } catch (IOException e) {
+                    message = String.format("IOException -%s- in -%s- with context -%s-", e.getMessage(), routine, context);
+                    Logger
+                            .getLogger(CLASS_NAME).log(Level.SEVERE, message);
                     throw new WebApplicationException(createResponse(ex, Response.Status.INTERNAL_SERVER_ERROR));
+
                 }
             }
             if (tempOutputFile != null) {
                 tempOutputFile.delete();
             }
+            message = String.format("IOException -%s- in -%s- with context -%s-", Response.Status.INTERNAL_SERVER_ERROR, routine, context);
+            Logger
+                    .getLogger(CLASS_NAME).log(Level.SEVERE, message);
             throw new WebApplicationException(createResponse(ex, Response.Status.INTERNAL_SERVER_ERROR));
         }
         process(lang, input, tempOutputFile);
@@ -78,31 +92,52 @@ public class TokenizerKafResource {
         // TCF can be sent as StreamingOutput from the TCF output temporary file
         return new OutPutWriter(tempOutputFile);
     }
-    @Path("plainget")
+
+    @Path("lrs")
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(TEXT_XML)
     public StreamingOutput tokenizeTextFromPlainFromUrl(@QueryParam("lang") String lang, @QueryParam("url") String theUrl, final InputStream input) {
+        String message;
+        String submessage;
+        String routine = "tokenizeTextFromPlainFromUrl";
+        message = String.format("Executing  -%s- in context -%s-\n", routine, context);
+        Logger
+                .getLogger(CLASS_NAME).log(Level.INFO, message);
         OutputStream tempOutputData = null;
-        System.err.println("***** FOR LRS KAF -GET INVOCATION:");
-        System.err.println("\t***** FOR LRS KAF -GET INVOCATION lang parameter :"+lang+ " ******");
-        System.err.println("\t***** FOR LRS KAF -GET INVOCATION url parameter :"+theUrl+ " ******");
-        System.err.println("***** FOR LRS KAF -GET INVOCATION END *****\n\n");
+        submessage = String.format("***** FOR LRS KAF -GET INVOCATION:\n\t"
+                + "***** FOR LRS KAF -GET INVOCATION lang parameter -%s- ******\n\t"
+                + "***** FOR LRS KAF -GET INVOCATION url parameter -%s- ******\n\t"
+                + "***** FOR LRS KAF -GET INVOCATION END *****\n\n", lang, theUrl);
+        message = String.format("-%s-\n", submessage);
+        Logger
+                .getLogger(CLASS_NAME).log(Level.INFO, message);
+//        System.err.println("***** FOR LRS KAF -GET INVOCATION:");
+//        System.err.println("\t***** FOR LRS KAF -GET INVOCATION lang parameter :" + lang + " ******");
+//        System.err.println("\t***** FOR LRS KAF -GET INVOCATION url parameter :" + theUrl + " ******");
+//        System.err.println("***** FOR LRS KAF -GET INVOCATION END *****\n\n");
         File tempOutputFile = null;
         try {
             tempOutputFile = File.createTempFile(TEMP_FILE_PREFIX, TEMP_FILE_SUFFIX);
-            
+
         } catch (IOException ex) {
             if (tempOutputData != null) {
                 try {
+                    
                     tempOutputData.close();
                 } catch (IOException e) {
+                    message = String.format("IOException -%s- in -%s- with context -%s-", Response.Status.INTERNAL_SERVER_ERROR, routine, context);
+                    Logger
+                            .getLogger(CLASS_NAME).log(Level.SEVERE, message);
                     throw new WebApplicationException(createResponse(ex, Response.Status.INTERNAL_SERVER_ERROR));
                 }
             }
             if (tempOutputFile != null) {
                 tempOutputFile.delete();
             }
+            message = String.format("IOException -%s- in -%s- with context -%s-", Response.Status.INTERNAL_SERVER_ERROR, routine, context);
+            Logger
+                    .getLogger(CLASS_NAME).log(Level.SEVERE, message);
             throw new WebApplicationException(createResponse(ex, Response.Status.INTERNAL_SERVER_ERROR));
         }
         processUrl(lang, theUrl, input, tempOutputFile);
@@ -111,51 +146,56 @@ public class TokenizerKafResource {
         // TCF can be sent as StreamingOutput from the TCF output temporary file
         return new OutPutWriter(tempOutputFile);
     }
+
     /**
-     * This method processes the plain text and creates a KAF document from the input provided.
-     * It calls the corresponding method from the tool.
+     * This method processes the plain text and creates a KAF document from the
+     * input provided. It calls the corresponding method from the tool.
+     *
      * @param lang the language used to load the module
      * @param input the input stream
      * @param out the output file
      */
     private void process(String lang, InputStream input, File out) {
-         try {
-             TokenizerKafCore tool = new TokenizerKafCore(lang);
-             tool.process(input);
-             PrintStream ps = new PrintStream(out);
-             tool.getResult().toKaf(ps);
-             
-         } catch (Exception ex) {
-            
+        try {
+            TokenizerKafCore tool = new TokenizerKafCore(lang);
+            tool.process(input);
+            PrintStream ps = new PrintStream(out);
+            tool.getResult().toKaf(ps);
+
+        } catch (Exception ex) {
+
             throw new WebApplicationException(createResponse(ex, Response.Status.INTERNAL_SERVER_ERROR));
         }
-        
+
     }
-    
+
     /**
-     * This method processes the plain text and creates a KAF document from the input provided.
-     * It calls the corresponding method from the tool.
+     * This method processes the plain text and creates a KAF document from the
+     * input provided. It calls the corresponding method from the tool.
+     *
      * @param lang the language used to load the module
      * @param input the input stream
      * @param out the output file
      */
-    private void processUrl(String lang, String theUrl,InputStream input, File out) {
-         try {
-             TokenizerKafCore tool = new TokenizerKafCore(lang);
-             input=new URL(theUrl).openStream();
-             tool.process(input);
-             PrintStream ps = new PrintStream(out);
-             tool.getResult().toKaf(ps);
-             
-         } catch (Exception ex) {
-            
+    private void processUrl(String lang, String theUrl, InputStream input, File out) {
+        try {
+            TokenizerKafCore tool = new TokenizerKafCore(lang);
+            input = new URL(theUrl).openStream();
+            tool.process(input);
+            PrintStream ps = new PrintStream(out);
+            tool.getResult().toKaf(ps);
+
+        } catch (Exception ex) {
+
             throw new WebApplicationException(createResponse(ex, Response.Status.INTERNAL_SERVER_ERROR));
         }
-        
+
     }
-    
+
     /**
-     * Private method to create the response depending on statuses and exceptions
+     * Private method to create the response depending on statuses and
+     * exceptions
+     *
      * @param ex the exception
      * @param status the status
      * @return the response
@@ -168,5 +208,5 @@ public class TokenizerKafResource {
         Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, message, ex);
         return Response.status(status).entity(message).type(MediaType.TEXT_PLAIN).build();
     }
-    
+
 }
