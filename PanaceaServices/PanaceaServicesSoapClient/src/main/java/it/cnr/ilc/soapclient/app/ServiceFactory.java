b047f1fe7f42fa17c5d213afe7767a065853edcd
riccardodg <riccardo.delgratta@ilc.cnr.it>
2017-07-24 12:27:36 +0200

    24/7/2017 11:00 Final with javadoc

--- PanaceaServices/PanaceaServicesSoapClient/src/main/java/it/cnr/ilc/soapclient/app/ServiceFactory.java
+++ PanaceaServices/PanaceaServicesSoapClient/src/main/java/it/cnr/ilc/soapclient/app/ServiceFactory.java
@@ -12,11 +12,16 @@ import it.cnr.ilc.soapclient.i.PanaceaService;
 import it.cnr.ilc.soapclient.impl.FreelingIt;
 
 /**
- *
+ * Factory to instantiate the correct service according to the input
  * @author Riccardo Del Gratta &lt;riccardo.delgratta@ilc.cnr.it&gt;
  */
 public class ServiceFactory {
 
+    /**
+     * Returns the panacea service
+     * @param service the name of the service
+     * @return the instantiated service
+     */
     public PanaceaService getService(String service) {
         if (service == null) {
             return null;
@@ -27,6 +32,11 @@ public class ServiceFactory {
         return null;
     }
     
+    /**
+     * Returns the class to read th input and fill basic types
+     * @param service the name of the service
+     * @return the instantiated class to read the service output 
+     */
     public FillSimpleTypes getFillSimpleType(String service){
     if (service == null) {
             return null;
--- PanaceaServices/PanaceaServicesSoapClient/src/main/java/it/cnr/ilc/soapclient/app/Theservice.java
+++ PanaceaServices/PanaceaServicesSoapClient/src/main/java/it/cnr/ilc/soapclient/app/Theservice.java
@@ -13,6 +13,7 @@ import java.util.logging.Logger;
 import org.apache.commons.validator.routines.UrlValidator;
 
 /**
+ * This class wraps the input and executes the service
  *
  * @author Riccardo Del Gratta &lt;riccardo.delgratta@ilc.cnr.it&gt;
  */
@@ -24,14 +25,18 @@ public class Theservice {
 
     public static final String CLASS_NAME = Theservice.class.getName();
 
+    /**
+     * Printhelp
+     */
     public void printHelp() {
         System.out.println("This tool wrappers the following Panacea services: " + Vars.services + " and can process both texts and URLs");
         System.out.println("In what follows, -sf and -m parameters depend on the service profile loaded. However, the program exits "
                 + "if supplied parameter are not correct");
         System.out.println("\n\nTexts or URLs can be provided in two ways");
 
-        System.out.println("1) Usage echo \"text_to_analize|URL\" | java -jar <code>.jar -s <service> [-o output_file] [-f <final_output>] [-sf <service_output>] [-m inputs]");
+        System.out.println("1) Usage echo \"text_to_analize|URL\" | java -jar <code>.jar -l lang -s <service> [-o output_file] [-f <final_output>] [-sf <service_output>] [-m inputs]");
         System.out.println("\twhere:");
+        System.out.println("\t\t the -l parameter is mandatory and specifies the language (in three characthers code)");
         System.out.println("\t\t the -s parameter is mandatory and MUST be one out of " + Vars.services);
         System.out.println("\t\t the -o parameter is optional  and tells the program to write the output in file");
         System.out.println("\t\t the -f parameter is optional  and tells the program to write the output in one of the following formats " + Vars.formats
@@ -46,6 +51,7 @@ public class Theservice {
         System.out.println("2) Usage java -jar <code>.jar -s <service> [-i input_file] [-o output_file] "
                 + "[-f <final_output>] [-sf <service_output>] [-m inputs] -r");
         System.out.println("\twhere:");
+        System.out.println("\t\t the -l parameter is mandatory and specifies the language (in three characthers code)");
         System.out.println("\t\t the -s parameter is mandatory and MUST be one out of " + Vars.services);
         System.out.println("\t\t the -i parameter is optional  and tells the program to read from the output in file");
         System.out.println("\t\t the -o parameter is optional  and tells the program to write the output in file");
@@ -59,15 +65,18 @@ public class Theservice {
         System.out.println("\n\n In case of 2) if no input_file is provided, the program waits for human input from keyboard");
         System.out.println("\n\n ******************************************");
         System.out.println("Example (for mode 1, just to see invocation...)");
-        System.out.println("echo \"Mi chiamo Alberto e abito a Pisa.\" | java -jar <code>.jar -s freeling_it -o /tmp/out -f kaf "
-                + "-sf token -m multiword=true, ner=basic");
+        System.out.println("echo \"Mi chiamo Alberto e abito a Pisa.\" | java -jar <code>.jar -l ita -s freeling_it -o /tmp/out -f kaf  "
+                + "-sf token -m multiword=true,ner=basic -t");
         System.out.println("where:");
+        System.out.println("\t -l ita specifies the 3 characters code language");
         System.out.println("\t -s freeling_it indicates that the service profile loaded is freeling_it");
         System.out.println("\t -o /tmp/out indicates that the result of the execution will be saved in a file");
         System.out.println("\t -f kaf indicates that the format of the result is in KAF");
         System.out.println("\t -sf token indicates that the service outputs its result as a list of tokens. "
                 + "Since the -f parameter is specified, the list of tokens is rendered as a KAF document.");
         System.out.println("\t -m multiword=true, ner=basic indicates additional parameters");
+        System.out.println("\t -t parameter is optional  and tells the program to read from temporary file instead that from the service "
+                + "URL which is the default");
         System.out.println("\n\n ******************************************");
         //System.out.println("\t\t\t -m\"multiword=true,ner=basic,output_format=token\"");
     }
@@ -93,8 +102,16 @@ public class Theservice {
 
     }
 
+    /**
+     * This method does the following:
+     * <ul>
+     * <li>Sets the input parameters as service inputs; </li>
+     * <li>Checks if the input string is a URL. If so then executes the corresponding methods; </li>
+     * <li>Executes the service. </li>
+     * </ul>
+     */
     public void run() {
-        String message = "";
+        String message = "", routine = "run";
         boolean fromUrl = false;
 
         service.setInputs(inputs);
@@ -102,12 +119,13 @@ public class Theservice {
         UrlValidator defaultValidator = new UrlValidator(); // default schemes
         if (defaultValidator.isValid(input)) {
             fromUrl = true;
-            message = String.format("The inputType supplied -%s- requires to be execued reading from a URL. So fromUrl is set to %s", input, fromUrl);
+            message = String.format("Routine %s. The inputType supplied -%s- requires to be read from a URL. "
+                    + "So fromUrl is set to %s", routine, input, fromUrl);
             Logger.getLogger(CLASS_NAME).log(Level.INFO, message);
 
         } else {
             fromUrl = false;
-            message = String.format("The inputType supplied -%s- requires to be execued reading from a String. So fromUrl is set to %s", input, fromUrl);
+            message = String.format("The inputType supplied -%s- requires to be read from a String. So fromUrl is set to %s", input, fromUrl);
             Logger.getLogger(CLASS_NAME).log(Level.INFO, message);
         }
 
@@ -119,11 +137,10 @@ public class Theservice {
 
         service.runService(input, inputs, fromUrl);
         if (service.getStatus() == 0 && !service.getOutputUrl().isEmpty()) {
-            message = String.format("Executed service %s with status %s and output url %s", 
+            message = String.format("Executed service %s with status %s and output url %s",
                     service.getSERVICE_NAME(), service.getStatus(), service.getOutputUrl());
             Logger.getLogger(CLASS_NAME).log(Level.INFO, message);
         }
-        
 
     }
 
