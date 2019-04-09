da09db32a065bbc0ea285b7254219323b536c1f3
Riccardo Del Gratta <riccardo.delgratta@gmail.com>
2019-04-09 17:32:01 +0200

    Quite done with Opener tokenizer. forservice left

--- IlcUtils/src/main/java/it/cnr/ilc/ilcutils/Vars.java
+++ IlcUtils/src/main/java/it/cnr/ilc/ilcutils/Vars.java
@@ -10,18 +10,42 @@ import java.util.List;
 
 /**
  * Variables. TODO: read properties from file
+ *
  * @author Riccardo Del Gratta &lt;riccardo.delgratta@ilc.cnr.it&gt;
  */
 public class Vars {
-    public static String version ="1.0";
-    public static boolean debug=false;
-    public static String SERVICE_DESR="desr";
-    public static String SERVICE_FR2CONLL_IT="fc_freeling_text_2_conll_it";
-    public static String FREELING_IT="freeling_it";
-    public static String OF_KAF="kaf";
-    public static String OF_TCF="tcf";
-    public static String OF_DEF="tabbed";
-    public static List<String> services = Arrays.asList(Vars.SERVICE_DESR, Vars.SERVICE_FR2CONLL_IT, Vars.FREELING_IT);
-    public static List<String> formats = Arrays.asList(Vars.OF_DEF, Vars.OF_TCF, Vars.OF_KAF);
-    public static String encoding="UTF-8";
+
+    public static String version = "1.0";
+    public static boolean debug = false;
+    public static String OF_KAF = "kaf";
+    public static String OF_TCF = "tcf";
+    public static String OF_JSON = "json";
+
+    public static String IF_KAF = "kaf";
+    public static String IF_RAW = "raw";
+
+    public static String ITA = "ita";
+    public static String ENG = "eng";
+    public static String FRA = "fra";
+    public static String ESP = "esp";
+    public static String DEU = "deu";
+    public static String NLD = "nld";
+    public static String IT = "it";
+    public static String EN = "en";
+    public static String FR = "fr";
+    public static String ES = "es";
+    public static String DE = "de";
+    public static String NL = "nl";
+
+    public static String SERVICE_DESR = "desr";
+    public static String SERVICE_FR2CONLL_IT = "fc_freeling_text_2_conll_it";
+    public static String FREELING_IT = "freeling_it";
+
+    public static String OF_DEF = "tabbed";
+    public static List<String> panaceaServices = Arrays.asList(Vars.SERVICE_DESR, Vars.SERVICE_FR2CONLL_IT, Vars.FREELING_IT);
+    public static List<String> panaceaFormats = Arrays.asList(Vars.OF_DEF, Vars.OF_TCF, Vars.OF_KAF);
+    public static List<String> openerFormats = Arrays.asList(Vars.OF_TCF, Vars.OF_KAF, Vars.OF_JSON);
+
+    public static List<String> iFormats = Arrays.asList(Vars.IF_KAF, Vars.IF_RAW);
+    public static String encoding = "UTF-8";
 }
