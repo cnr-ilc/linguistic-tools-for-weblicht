ae6c77e8115b6e4a8386ff763838b0a68b1edfae
Riccardo Del Gratta <riccardo.delgratta@gmail.com>
2019-04-11 15:21:56 +0200

    Added javadoc

--- IlcIOUtils/src/main/java/it/cnr/ilc/ilcioutils/IlcInputToFile.java
+++ IlcIOUtils/src/main/java/it/cnr/ilc/ilcioutils/IlcInputToFile.java
@@ -7,10 +7,12 @@ package it.cnr.ilc.ilcioutils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 /**
  *Some utilities to read from an inputstream and write a file
@@ -20,6 +22,7 @@ public class IlcInputToFile {
 
     
     private static final String TEMP_FILE_PREFIX = "out_from_service_";
+    private static final String TEMP_FILE_IS_PREFIX = "out_from_is_";
     private static final String TEMP_FILE_SUFFIX = ".txt";
     
     /**
@@ -66,6 +69,30 @@ public class IlcInputToFile {
           
         } catch (IOException e) {
             message = String.format("Error in accessing String %s %s", string, e.getMessage());
+            Logger.getLogger(CLASS_NAME).log(Level.SEVERE, message);
+        }
+        return tempOutputFile;
+       
+    }
+     /**
+     * Creates a file with the content read from string
+     * @param input the InputStream containing the content
+     * @return a File with the content from the URL
+     */
+     public static File createAndWriteTempFileFromInputStream(InputStream input) {
+        File tempOutputFile=null;
+        String encoding="UTF-8";
+        String string; 
+        String message;
+        try {
+          string = IOUtils.toString(input, "UTF-8"); 
+          tempOutputFile   = File.createTempFile(TEMP_FILE_IS_PREFIX, TEMP_FILE_SUFFIX);
+          FileUtils.writeStringToFile(tempOutputFile, string, encoding);
+           
+          
+          
+        } catch (IOException e) {
+            message = String.format("Error in accessing InputStream %s %s", input.toString(), e.getMessage());
             Logger.getLogger(CLASS_NAME).log(Level.SEVERE, message);
         }
         return tempOutputFile;
